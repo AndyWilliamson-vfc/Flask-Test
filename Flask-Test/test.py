4371477e37d184ce91e72d3aa427feeebea06f7d
AndyWilliamson-vfc <55087277+AndyWilliamson-vfc@users.noreply.github.com>
2020-08-24 15:38:23 +0100

    set environment and save test.py

---
+++ Flask-Test/test.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello you lovely world!"
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
+
