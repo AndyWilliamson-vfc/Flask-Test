26f21c47f08efbd97e24706903894379f6fc329f
AndyWilliamson-vfc <55087277+AndyWilliamson-vfc@users.noreply.github.com>
2020-08-25 10:49:58 +0100

    testing

--- Flask-Test/app.py
+++ Flask-Test/app.py
@@ -3,7 +3,10 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template 
+import pyodbc
+
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -14,6 +17,17 @@ wsgi_app = app.wsgi_app
 def hello():
     """Renders a sample page."""
     return render_template("/index.html")
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=WDE01010;'
+                      'Database=Dickies_BI_UK_Staging;'
+                      'Trusted_Connection=yes;')
+
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM raw_INP15 where cono15="DC"')
+
+for row in cursor:
+    print(row)
 
 if __name__ == '__main__':
     import os
